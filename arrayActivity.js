b84997a5326356bf1eb030e64f902ddf1ce0f60d
unknown <nshienarocell@gmail.com>
2024-10-20 06:21:30 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,11 @@
+//Favorite Singer
+let favSingers = ["Ben&Ben", "Moira Dela Torre", "Taylor Swift"];
+console.log("My first favorite singer is", favSingers[0]);
+
+// Favorite Numbers
+let favNumbers = ["2", "25", "7", "28"];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
